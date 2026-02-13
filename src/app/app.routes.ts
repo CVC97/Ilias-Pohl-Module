79320e2f99769fe71a12ce7abcb022c1e039530b
CVC97 <cvcarnap@gmail.com>
2026-02-13 17:11:09 +0100

    Pohl: Init commit.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Glossary } from './features/glossary/glossary';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Home },
+	{ path: 'glossary', component: Glossary }
+];
---
+++ src/app/features/glossary/glossary.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-glossary',
+	standalone: true,
+	imports: [],
+	templateUrl: './glossary.html',
+	styleUrl: './glossary.css',
+})
+export class Glossary {
+
+}
